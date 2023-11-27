ba3e5a708a26e0330a9adfb58d3b07d397d236b0
Farida Yasmeen <faridayasmeencse@gmail.com>
2023-11-27 17:04:15 +0600

    added lottie animation

--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -1,97 +1,43 @@
-import { useContext, useState } from "react";
-import { AuthContext } from "../providers/AuthProvider";
-import { Link, useLocation, useNavigate } from "react-router-dom";
-
-
+import Lottie from 'lottie-react';
+import working from '../../../public/working.json'
 
 const Login = () => {
 
-    const {signIn} = useContext(AuthContext);
-    const location = useLocation();
-    const navigate = useNavigate();
-    console.log('location in the login page',location);
 
-    const [loginError,setLoginError] = useState('');
-    const [loginSuccess,setLoginSuccess] = useState('');
-
-    const handleLogin = e =>{
-        e.preventDefault();
-        console.log(e.currentTarget);
-        const form = new FormData(e.currentTarget);
-        const email = form.get('email');
-        const password = form.get('password');
-        signIn(email,password)
-        .then(result =>{
-            console.log(result.user)
-            // navigate after login 
-            navigate(location?.state ? location.state : '/' );
-            setLoginSuccess('user login successfully')
-        })
-        .catch(error =>{
-            setLoginError(error.message);
-        })
-    }
 
     return (
-        <div className="box-border bg-violet-200 pb-10 rounded-xl shadow-xl lg:mt-20 mb-16">
-            <h2 className="text-3xl text-center font-semibold py-5">Please Login Here!</h2>
-            <form onSubmit={handleLogin} className="w-3/4 lg:w-1/2 mx-auto">
-                <div className="form-control">
-                    <label className="label">
-                        <span className="label-text font-semibold">Name</span>
-                    </label>
-                    <input type="text" 
-                        name="name"
-                        placeholder="Name" 
-                        className="input input-bordered" 
-                        required />
+        <div className="hero min-h-screen bg-base-200">
+            <div className="hero-content flex-col lg:flex-row-reverse">
+            <div className="text-center lg:text-left">
+                <h1 className="text-5xl font-bold my-4">Login now!</h1>
+                <div>
+                    <Lottie animationData={working} />
                 </div>
-                {/* <div className="form-control">
-                    <label className="label">
-                        <span className="label-text font-semibold">Photo URL</span>
-                    </label>
-                    <input type="text" 
-                        name="photo"
-                         placeholder="Photo" 
-                        className="input input-bordered" 
-                        required />
-                </div> */}
+            </div>
+            <div className="card shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
+                <form className="card-body">
                 <div className="form-control">
                     <label className="label">
-                        <span className="label-text font-semibold">Email</span>
+                    <span className="label-text">Email</span>
                     </label>
-                    <input type="email" 
-                           name="email"
-                           placeholder="email" 
-                           className="input input-bordered" 
-                           required />
+                    <input type="email" placeholder="email" className="input input-bordered" required />
                 </div>
                 <div className="form-control">
                     <label className="label">
-                        <span className="label-text font-semibold">Password</span>
+                    <span className="label-text">Password</span>
                     </label>
-                    <input type="password" 
-                           name="password"
-                           placeholder="password" 
-                           className="input input-bordered" 
-                           required />
+                    <input type="password" placeholder="password" className="input input-bordered" required />
+                    <label className="label">
+                    <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
+                    </label>
                 </div>
-                {/* <div className="form-control mt-6">
+                <div className="form-control mt-6">
                     <button className="btn btn-primary">Login</button>
-                    <button onClick={handleGoogleSignIn} className="btn flex items-center justify-center gap-3 m-3">
-                        <img className="w-10 h-10 rounded-full" 
-                            src={googleImg} alt="" />Login With Google</button>
-                </div> */}
-            </form>
-                <p className="text-center m-5">Do not have an account?
-                     <Link className="to-blue-600 font-bold ml-3" to='/register'>Register</Link></p>
-                {
-                    loginError && <p className="text-red-600 text-center">{loginError}</p>
-                }
-                {
-                    loginSuccess && <p className="text-green-600 text-center">{loginSuccess}</p>
-                }
-        </div>
+                </div>
+                </form>
+            </div>
+            </div>
+      </div>
     );
 };
 
--- src/Pages/Register/Register.jsx
+++ src/Pages/Register/Register.jsx
@@ -1,16 +1,16 @@
 import { useContext } from "react";
-import useAxiosPublic from "../../hooks/useAxiosPublic";
+// import useAxiosPublic from "../../hooks/useAxiosPublic";
 import { AuthContext } from "../providers/AuthProvider";
 import { useForm } from "react-hook-form";
 import Swal from "sweetalert2";
 import { Helmet } from "react-helmet-async";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 
 
 const Register = () => {
 
-    const axiosPublic = useAxiosPublic();
+    // const axiosPublic = useAxiosPublic();
     const { register, handleSubmit, reset, formState: { errors } } = useForm();
     const { createUser, updateUserProfile } = useContext(AuthContext);
     const navigate = useNavigate();
@@ -42,13 +42,13 @@ const Register = () => {
     return (
         <>
              <Helmet>
-                <title>Bistro Boss | Sign Up</title>
+                <title>Times Talk | Register</title>
             </Helmet>
             <div className="hero min-h-screen bg-base-200">
                 <div className="hero-content flex-col lg:flex-row-reverse">
                     <div className="text-center lg:text-left">
-                        <h1 className="text-5xl font-bold">Sign up now!</h1>
-                        <p className="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
+                        <h1 className="text-5xl font-bold">Register now!</h1>
+                        {/* <p className="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p> */}
                     </div>
                     <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
                         <form onSubmit={handleSubmit(onSubmit)} className="card-body">
@@ -85,11 +85,11 @@ const Register = () => {
                                 })} placeholder="password" className="input input-bordered" />
                                 {errors.password?.type === 'required' && <p className="text-red-600">Password is required</p>}
                                 {errors.password?.type === 'minLength' && <p className="text-red-600">Password must be 6 characters</p>}
-                                {errors.password?.type === 'maxLength' && <p className="text-red-600">Password must be less than 20 characters</p>}
+                                {/* {errors.password?.type === 'maxLength' && <p className="text-red-600">Password must be less than 20 characters</p>} */}
                                 {errors.password?.type === 'pattern' && <p className="text-red-600">Password must have one Uppercase one lower case, one number and one special character.</p>}
-                                <label className="label">
+                                {/* <label className="label">
                                     <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
-                                </label>
+                                </label> */}
                             </div>
                             <div className="form-control mt-6">
                                 <input className="btn btn-primary" type="submit" value="Sign Up" />
