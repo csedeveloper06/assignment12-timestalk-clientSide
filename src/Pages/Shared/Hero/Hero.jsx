05d8a81cb1b549cbe1290935858afa3be20d2cc0
Farida Yasmeen <faridayasmeencse@gmail.com>
2023-11-30 09:14:53 +0600

    set up the react parallax to the Subscription banner

--- src/Pages/Shared/Hero/Hero.jsx
+++ src/Pages/Shared/Hero/Hero.jsx
@@ -1,17 +1,24 @@
+import { Parallax } from 'react-parallax';
 
-
-const Hero = ({img}) => {
+const Hero = ({img,title}) => {
     return (
-        <div className="hero min-h-screen" style={{backgroundImage: `url(${img})`}}>
+        <Parallax
+            blur={{ min: -50, max: 50 }}
+            bgImage={img}
+            bgImageAlt="Payment"
+            strength={-200}
+        >
+          <div className="hero h-[600px]" >
             <div className="hero-overlay bg-opacity-60"></div>
             <div className="hero-content text-center text-neutral-content">
                 <div className="max-w-md">
-                    <h1 className="mb-5 text-5xl font-bold">Hello there</h1>
+                    <h1 className="mb-5 text-5xl font-bold uppercase">{title}</h1>
                     <p className="mb-5">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
-                    <button className="btn btn-primary">Get Started</button>
                 </div>
             </div>
-      </div>
+         </div>
+       </Parallax>
+      
     );
 };
 
--- src/Pages/SubsCription/SubsCription.jsx
+++ src/Pages/SubsCription/SubsCription.jsx
@@ -1,6 +1,6 @@
 import { Helmet } from "react-helmet";
 import Hero from "../Shared/Hero/Hero";
-import subscriptionImage from '../../../public/assets/subscription3.jpg'
+import subscriptionImage from '../../../public/assets/subscription2.avif'
 
 const SubsCription = () => {
     return (
@@ -8,7 +8,8 @@ const SubsCription = () => {
              <Helmet>
                 <title>Times Talk | Subscriptions</title>
             </Helmet>
-            <Hero img={subscriptionImage}></Hero>
+            <Hero img={subscriptionImage} title="Payment"></Hero>
+            <h1 className="text-3xl">Subscription Area</h1>
         </div>
     );
 };
